660b0dd3e58f0f7466d79baa02c972983e6300a2
gabrielpramos <ramos.gabriel.pereira@gmail.com>
2020-09-08 21:04:02 -0300

    Att routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter, Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
-import Routes from './Routes';
+import Authenticate from './loggedIn/pages/Authenticate/index';
+import Dashboard from './loggedIn/pages/Dashboard/components/addInspection/index';
+import Inspection from './loggedIn/pages/Inspection/index';
 
 export default () => {
   return (
     <div>
-      <Routes />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Authenticate} />
+          <Route path='/dashboard' exact component={Dashboard} />
+          <Route path='/inspection:id' component={Inspection} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
